b24f02485a3a71156d0d00cd3ceadd7908492081
NorthernL1ghts <34502005+NorthernL1ghts@users.noreply.github.com>
2025-08-30 10:30:08 +0100

    Implemented GLFW Window Event Dispatch

--- Maple/src/Maple/Application.cpp
+++ Maple/src/Maple/Application.cpp
@@ -1,20 +1,29 @@
 ﻿#include "mppch.h"
 #include "Application.h"
 
-#include "Maple/Events/ApplicationEvent.h"
 #include "Maple/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Maple {
+	#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		MP_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
@@ -27,4 +36,9 @@ namespace Maple {
 		}
 	}
 
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
+	}
 }
--- Maple/src/Maple/Application.h
+++ Maple/src/Maple/Application.h
@@ -3,6 +3,7 @@
 #include "Maple/Core.h"
 #include "Maple/Events/Event.h"
 #include "Maple/Window.h"
+#include "Maple/Events/ApplicationEvent.h"
 
 namespace Maple {
 
@@ -13,7 +14,9 @@ namespace Maple {
 		virtual ~Application();
 
 		void Run();
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
 		bool m_Running = true;
 		std::unique_ptr<Window> m_Window;
 	};
